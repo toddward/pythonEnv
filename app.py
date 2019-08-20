52a352a11ecedfc081765e3af9ca5b4ab631e122
Todd Wardzinski <wardzinski.todd@gmail.com>
2019-08-20 14:23:13 -0400

    updating ports

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(response.getvalue())
 
 
-httpd = HTTPServer(('localhost', 80), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)
 httpd.serve_forever()
